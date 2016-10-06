07b31dfea1f8a3fdef28a1fe123dbd4779a1a8f3
Atul Bansode <abansode@outlook.com>
2016-10-06 17:51:00 +0530

    Rename the power.rc, move the sensor config to target.rc for better management

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -50,7 +50,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.power.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.power_msm8916.rc
+LOCAL_SRC_FILES    := etc/init.qcom.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -75,7 +75,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.power_msm8916.rc
+LOCAL_SRC_FILES    := etc/init.qcom.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
