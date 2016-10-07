5a440ca79eebf592a7043216b821226f8bb0a65e
Paras Nagda <pnagda@codeaurora.org>
2016-10-07 19:00:39 +0530

    nx512j: mediacodec: Allow select and eventfd syscalls for 8916
    Add pselect6 and eventfd2 syscalls
    Includes following:
    mediacodec: allow sendto and recvfrom syscalls
    mediacodec: allow lseek syscall
    mediaextractor: allow pread64 and readlinkat syscalls
    
    Change-Id: Iff6e2f7fc0562a520f56cbf4bbce72f1af545f9c

--- product/media.mk
+++ product/media.mk
@@ -15,3 +15,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1
     persist.sys.wfd.virtual=0
+
+# Seccomp
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:system/vendor/etc/seccomp_policy/mediaextractor.policy
+
