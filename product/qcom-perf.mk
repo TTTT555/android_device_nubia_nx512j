be83a1446a630b676779351e5fbd1d947075e148
Kilian von Pflugk <kilian@nood.tv>
2017-01-26 22:25:24 +0100

    Add core_ctl props
    * perfd uses them to restore proper min/max cpus values
    
    Change-Id: I39e910afc48c869f7796da9c1ed78996798cf427

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -8,7 +8,9 @@ PRODUCT_PACKAGES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.core_ctl_min_cpu=2 \
+    ro.core_ctl_max_cpu=4
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.min_freq_0=200000 \
