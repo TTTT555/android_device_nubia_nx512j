81d8c69af7abbc5bd468ec063d72b61a01a1f733
Bauuuuu <abansode@outlook.com>
2017-02-10 17:07:32 +0100

    remove the keyhandler the dt2w handled by kernel now
    Change-Id: Ia203626df635bb79067edaa83e8ab0a461a75bda

--- product/keyhandler.mk
+++
@@ -1,8 +0,0 @@
-# KeyHandler
-PRODUCT_PACKAGES += \
-    com.cyanogenmod.keyhandler
-
-PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
-
-#Prevent the dexopt of the keyhandler --> extra guard
-$(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
