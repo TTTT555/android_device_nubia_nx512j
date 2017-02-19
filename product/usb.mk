ddb78f1945166fe198c6b46891c2152068d35fad
Bauuuuu <abansode@outlook.com>
2017-02-19 17:20:14 +0100

    add the missed USB peripheral files

--- product/usb.mk
+++ product/usb.mk
@@ -1,7 +1,8 @@
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
 
 # USB ID
 ADDITIONAL_DEFAULT_PROPERTIES += \
