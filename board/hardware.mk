3a50943208deb3e013b447986dec44529966804b
Bauuuuu <abansode@outlook.com>
2017-06-06 00:16:32 +0200

    real time battery charging values
    Change-Id: I4706099440bcea9bd74a552b095d0710ccbb1ef7

--- board/hardware.mk
+++ board/hardware.mk
@@ -3,3 +3,6 @@ BOARD_HARDWARE_CLASS += device/nubia/nx512j/cmhw
 BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
 BOARD_USES_CYANOGEN_HARDWARE := true
 TARGET_TAP_TO_WAKE_NODE := "/data/tp/easy_wakeup_gesture"
+
+#Real time battery charging 
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
