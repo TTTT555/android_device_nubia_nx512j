0edbd9eb1ee31b498169c17ad1e19d1bc079efbf
Bauuuuu <abansode@outlook.com>
2017-06-18 11:37:35 +0200

    core_ctl: restore stock values all these tests have no great improvements on battery life so restore the good old values taken from oppo-msm8939-common
    Change-Id: If8e86c366c0d0873f70b27eba51370a1d35c45db

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -13,5 +13,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.core_ctl_max_cpu=4
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.min_freq_0=200000 \
-    ro.min_freq_4=200000
+    ro.min_freq_0=960000 \
+    ro.min_freq_4=800000
