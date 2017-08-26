c4d1cb91026133e7a2f1b928f024c71d04d7086f
Bauuuuu <abansode@outlook.com>
2017-08-26 22:58:34 +0200

    nx512j: rebrand vendor to lineage
    Change-Id: I490a9d07d299919588796e156b842a2f4f5665ff

--- lineage.mk
+++ lineage.mk
@@ -15,8 +15,8 @@
 
 $(call inherit-product, device/nubia/nx512j/full_nx512j.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_nx512j
 BOARD_VENDOR := nubia
