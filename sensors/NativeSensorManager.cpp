e42c325a213b871985caaac13e37b682a5ec201a
Bauuuuu <abansode@outlook.com>
2017-09-07 18:30:50 +0200

    nx512j: remove unsupported pressure sensor
    Change-Id: Ic1b2af480d76feaaa6fe67dfb089e4ec30c7acca

--- sensors/NativeSensorManager.cpp
+++ sensors/NativeSensorManager.cpp
@@ -478,9 +478,6 @@ int NativeSensorManager::getDataInfo() {
 				list->driver = new GyroSensor(list);
 				sensor_gyro = *(list->sensor);
 				break;
-			case SENSOR_TYPE_PRESSURE:
-				list->driver = new PressureSensor(list);
-				break;
 			case SENSOR_TYPE_SIGNIFICANT_MOTION:
 				list->driver = new SmdSensor(list);
 				break;
