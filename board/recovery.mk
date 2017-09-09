da54c416cfed72755fc86ce7fbeb9351f2352d91
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-08 17:48:17 -0700

    nx512j: Update for early-mounted system partition
     * This removes /system entry from fstab, since this partition
       is now early-mounted and the fstab entry is specified in device tree.
    
     * At the same time, also include a fully populated fstab, in order to avoid
       build breakage while generating recovery updater scripts.
    
    Change-Id: I8e558458b984e97a45a920c3751a6b7c3f2ebc5c

--- board/recovery.mk
+++ board/recovery.mk
@@ -1,5 +1,5 @@
 # Recovery
 TARGET_RECOVERY_DENSITY := xxhdpi
-TARGET_RECOVERY_FSTAB := device/nubia/nx512j/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := device/nubia/nx512j/rootdir/etc/fstab.full
 TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_cm
