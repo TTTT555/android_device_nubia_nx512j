e91b761a570c1f76166be3a3999dc86473d51260
Bauuuuu <abansode@outlook.com>
2017-09-09 07:33:39 +0200

    nx512j: update wifi configuration
    * wifi module now built in kernel
    
    Change-Id: I48381daba9c839379302e638cc8a3d6388563f26

--- board/qcom-wlan.mk
+++ board/qcom-wlan.mk
@@ -1,9 +1,4 @@
-# Wifi
-TARGET_PROVIDES_WCNSS_QMI := true
-
-# The uncompressed arm64 is too large, split wifi for now
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
+#wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
 BOARD_HOSTAPD_DRIVER := NL80211
