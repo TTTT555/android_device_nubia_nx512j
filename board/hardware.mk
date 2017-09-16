f916b48b8d39c27c49b7a3fe4d9e192258b238ee
Bauuuuu <abansode@outlook.com>
2017-09-16 23:00:57 +0200

    nx512j: rebrand to lineageos
    * cmhw -> lineagehw
    * rebrand blob extractions scripts
    
    Change-Id: I12e79b8f949d71ad44d3df8c5ba78d605497aaca

--- board/hardware.mk
+++ board/hardware.mk
@@ -1,7 +1,8 @@
-# CMHW
-BOARD_HARDWARE_CLASS += device/nubia/nx512j/cmhw
-BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
-BOARD_USES_CYANOGEN_HARDWARE := true
+# lineagehw
+BOARD_HARDWARE_CLASS += \
+    $(VENDOR_PATH)/lineagehw
+
+# double tap to wake gesture
 TARGET_TAP_TO_WAKE_NODE := "/data/tp/easy_wakeup_gesture"
 
 #Real time battery charging 
