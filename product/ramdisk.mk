efa95f86d66dc30180287d9f337b00e042abae63
Ethan Chen <intervigil@gmail.com>
2018-02-05 12:36:06 +0100

    nx512j: Remove init.qcom.bt.sh
    Change-Id: Iae343102be781e9fb985a14d13c466b3a997d860

--- product/ramdisk.mk
+++ product/ramdisk.mk
@@ -1,6 +1,5 @@
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.crda.sh \
-    init.qcom.bt.sh \
     init.qcom.coex.sh \
     init.qcom.fm.sh
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -30,13 +30,6 @@ include $(BUILD_PREBUILT)
 # Configuration scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.bt.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.bt.sh
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.mem.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
