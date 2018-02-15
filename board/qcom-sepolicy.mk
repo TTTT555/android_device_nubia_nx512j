f520dc426c656a0179220db3b1cc26fd1f87f771
Bauuuuu <abansode@outlook.com>
2018-02-15 16:14:14 +0100

    nx512j: sepolicy: initial O update
    Change-Id: I554693fb12d55ee22c79ffac27098bb6cd9a296a

--- board/qcom-sepolicy.mk
+++ board/qcom-sepolicy.mk
@@ -1,1 +1,6 @@
+#SElinux
 include device/qcom/sepolicy/sepolicy.mk
+
+BOARD_SEPOLICY_DIRS += \
+    device/nubia/nx512j/sepolicy
+
--- board/sepolicy.mk
+++
@@ -1,4 +0,0 @@
-# SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/nubia/nx512j/sepolicy \
-    $(VENDOR_PATH)/sepolicy
