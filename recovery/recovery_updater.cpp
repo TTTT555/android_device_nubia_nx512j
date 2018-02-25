882a06e6f5426ad8652b7242a0175c296aaa2f1c
jrior001 <jriordan001@gmail.com>
2018-02-25 18:58:47 -0500

    nx512j: Use real path in fstab
    Change-Id: Iaf0f6ff77547f635a98b61e19fc3f5f36489c504

--- recovery/recovery_updater.cpp
+++ recovery/recovery_updater.cpp
@@ -36,7 +36,7 @@
 #define ALPHABET_LEN 256
 #define KB 1024
 
-#define TZ_PART_PATH "/dev/block/bootdevice/by-name/tz"
+#define TZ_PART_PATH "/dev/block/platform/soc.0/7824900.sdhci/by-name/tz"
 #define TZ_VER_STR "QC_IMAGE_VERSION_STRING="
 #define TZ_VER_STR_LEN 24
 #define TZ_VER_BUF_LEN 255
