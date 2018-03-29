89758a33cc6d122093fba8ea562adc6c50219441
jrior001 <jriordan001@gmail.com>
2018-03-29 18:03:54 +0200

    nx512j: move blobs to vendor
    * also move proprietary-files-qc.txt ->proprietary-files.txt
    * Requires following hex edit to load configs from vendor:
    sed -i "s|/system/etc/data/dsi_config.xml|/vendor/etc/data/dsi_config.xml|g" vendor/lib64/libdsi_netctrl.so
    sed -i "s|/system/etc/data/dsi_config.xml|/vendor/etc/data/dsi_config.xml|g" vendor/lib/libdsi_netctrl.so
    sed -i "s|/system/etc/data/netmgr_config.xml|/vendor/etc/data/netmgr_config.xml|g" vendor/bin/netmgrd
    * Also requires edit of qcnvitems.xml and qcrilhook.xml
      to load jars from vendor
    
    Change-Id: I3798c23178bd8f86c2d39947bb90adb96f721508

--- board/shim.mk
+++ board/shim.mk
@@ -1,6 +1,6 @@
 # SHIM
 TARGET_LD_SHIM_LIBS := /system/vendor/lib/libqomx_jpegenc.so|libboringssl-compat.so \
-    /system/lib/hw/camera.msm8916.so|libzte_camera.so \
+    /system/vendor/lib/hw/camera.msm8916.so|libzte_camera.so \
     /system/vendor/lib64/libizat_core.so|libshim_gps.so \
     /system/vendor/lib64/libizat_core.so|libshims_flp.so
 
