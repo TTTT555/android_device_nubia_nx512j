9228c2a7c13bb070bc0f4c018570e39a380d1860
Bauuuuu <abansode@outlook.com>
2018-04-03 12:50:01 +0200

    nx512j: add build fingerprint from Unicom-v9.54 release
    Change-Id: Ib7f237aad7c7bf53b8b7f26fb7cd63f480271fbb

--- lineage.mk
+++ lineage.mk
@@ -18,13 +18,21 @@ $(call inherit-product, device/nubia/nx512j/full_nx512j.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifiers
 PRODUCT_NAME := lineage_nx512j
-BOARD_VENDOR := nubia
 PRODUCT_DEVICE := nx512j
+PRODUCT_MANUFACTURER := Nubia
+PRODUCT_BRAND := Nubia
+PRODUCT_MODEL := Nubia Z9 Max
 
 PRODUCT_GMS_CLIENTID_BASE := android-nubia
 
 TARGET_VENDOR_PRODUCT_NAME := NX512J
 TARGET_VENDOR_DEVICE_NAME := NX512J
+
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=NX512J PRODUCT_NAME=NX512J
 
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="NX512J-user 5.1.1 LMY47V eng.nubia.20171018.105347 release-keys"
+
+BUILD_FINGERPRINT := nubia/NX512J/NX512J:5.1.1/LMY47V/eng.nubia.20171018.105347:user/release-keys
