a95925b5b168307c201cc45a5660ebd6561ebd20
TTTT555 <albukerko@gmail.com>
2018-07-19 20:28:24 +0200

    Deleted non used hwui

--- product/display.mk
+++ product/display.mk
@@ -9,9 +9,6 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
 
-# Dalvik
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
-
 # OpenGLES
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
