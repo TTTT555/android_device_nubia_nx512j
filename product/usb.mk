b5189c87e31e9a11c86a8c0a799bab37ce922fa7
lcolitti <lcolitti@gmail.com>
2018-09-11 00:05:20 +0200

    Remove explicit Ethernet support, the device supports USB OTG.

--- product/usb.mk
+++ product/usb.mk
@@ -4,7 +4,6 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/vendor/etc/permissions/android.hardware.ethernet.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/vendor/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/vendor/etc/permissions/android.hardware.usb.accessory.xml
 
