3987fb4c3781643f6b585a8d1af6e048e63bd97b
TTTT555 <albukerko@gmail.com>
2018-11-02 19:01:13 +0200

    Optimize CPU performance and power save Tweak after binderized QTI HAL. In standby mode, two small cores at 200 MHz. Big cores also start at 200 MHz.

--- product/qcom-perf.mk
+++ product/qcom-perf.mk
@@ -8,10 +8,4 @@ PRODUCT_PACKAGES += \
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    ro.core_ctl_min_cpu=2 \
-    ro.core_ctl_max_cpu=4
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.min_freq_0=960000 \
-    ro.min_freq_4=800000
+    ro.vendor.extension_library=libqti-perfd-client.so
