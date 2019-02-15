Priyabrat1717/android_device_nubia_nx511j

Make ressurected

rr.mk
@@ -15,11 +15,11 @@
 
 $(call inherit-product, device/nubia/nx512j/full_nx512j.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifiers
-PRODUCT_NAME := lineage_nx512j
+PRODUCT_NAME := rr_nx512j
 PRODUCT_DEVICE := nx512j
 PRODUCT_MANUFACTURER := Nubia
 PRODUCT_BRAND := Nubia

TTTT555/android_device_nubia_nx512j

rr.mk
@@ -15,11 +15,11 @@
 
 $(call inherit-product, device/nubia/nx511j/full_nx511j.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Device identifiers
-PRODUCT_NAME := lineage_nx511j
+PRODUCT_NAME := rr_nx511j
 PRODUCT_DEVICE := nx511j
 PRODUCT_MANUFACTURER := Nubia
 PRODUCT_BRAND := Nubia